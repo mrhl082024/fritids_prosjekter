da38f1f6e1bf8cdf53b28ba5fabeafab4bbadee3
mrhl082024 <mhrl082024@gmail.com>
2024-10-01 14:12:10 +0200

    added some text

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Congratulations!");
+        Console.Write("You wrote your first lines of code.");
     }
 }
